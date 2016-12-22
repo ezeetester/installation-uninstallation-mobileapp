6d37c48d78c353d9c6376b6d6211fe19c844c7e0
Prasad BG <ezeetester@users.noreply.github.com>
2016-12-22 09:16:09 +0530

    Create installtest.sh
    This is a source file, you need to add specifics of your app, app path as shown in the script

---
+++ installtest.sh
@@ -0,0 +1,30 @@
+#!/bin/sh/
+#===============================================================================
+#
+#          FILE:  installtest.sh
+#
+#         USAGE:  sh installtest.sh <number of times you would like to install and uninstall your app ex: 1, 2 or a positive  number>
+#
+#   DESCRIPTION: This shell script is used to install and uninstall an Android app (apk) a number of times (number decided by the user)
+				  
+#
+#       OPTIONS:  --- Press Ctrl+C to end the script prematurely
+#  REQUIREMENTS:  --- Windows 10, Cygwin 64 (package dos2unix), Android SDK, Android Handset with Kit-Kat
+#          BUGS:  --- 
+#         NOTES:  ---
+#        AUTHOR:  Prasad BG, ezeetester@gmail.com
+#      
+#       VERSION:  1.0
+#       CREATED:  12/21/2016 11:15:01 PM IST
+#      REVISION:  ---
+#===============================================================================
+
+for((i=1; i<=$1; i++)); 
+ do
+    echo $i
+	cd </cygdrive/c/path-from-C-Drive-to-your-apk-file>
+	adb install <name-of-your-apk-file.apk>
+	adb uninstall <package-name-of-your-android-app>
+	
+   
+done
